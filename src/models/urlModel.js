d1720ca76b81d3945f40f413e699c60a9fc0b274
swarupachakraborty <swarupachakraborty01@gmail.com>
2022-05-17 15:20:31 +0530

    url model

--- src/models/urlModel.js
+++ src/models/urlModel.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
 
-const urlSchema = mongoose.Schema(
-    {
 
-    }
-);
+    const urlSchema=  mongoose.Schema({
+        urlCode: { type:String,
+                    required:true, 
+                    unique:true, 
+                    lowercase:true, 
+                    trim:true }, 
+        longUrl: { type:String,
+                   required:true}, 
+        shortUrl: {type:String,
+                    required:true, 
+                    unique:true}
+    }, {timestamps:true})
 
-module.exports=mongoose.model('urls',urlSchema);
+    module.exports=mongoose.models('urls',urlSchema)
+    
+
+    
