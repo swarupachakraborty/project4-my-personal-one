bcd3cc7a23b284ed9b1802995f00b56e88ad0f12
AbiMangalan <86941986+AbiMangalan@users.noreply.github.com>
2022-05-17 17:41:24 +0530

    Abi M - urlModel.js

--- src/models/urlModel.js
+++ src/models/urlModel.js
@@ -1,20 +1,25 @@
 const mongoose = require('mongoose');
 
+const urlSchema=  mongoose.Schema(
+    {
+        urlCode :   {  
+                        type:String,
+                        required:true, 
+                        unique:true, 
+                        lowercase:true, 
+                        trim:true   
+                    }, 
+        longUrl :   {  
+                        type:String,
+                        required:true   
+                    }, 
+        shortUrl :  { 
+                        type:String,
+                        required:true, 
+                        unique:true
+                    }
+    },
+    {timestamps:true}
+);
 
-    const urlSchema=  mongoose.Schema({
-        urlCode: { type:String,
-                    required:true, 
-                    unique:true, 
-                    lowercase:true, 
-                    trim:true }, 
-        longUrl: { type:String,
-                   required:true}, 
-        shortUrl: {type:String,
-                    required:true, 
-                    unique:true}
-    }, {timestamps:true})
-
-    module.exports=mongoose.models('urls',urlSchema)
-    
-
-    
+module.exports=mongoose.models('urls',urlSchema);
